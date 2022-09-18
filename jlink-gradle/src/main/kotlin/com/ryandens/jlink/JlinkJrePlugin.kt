20349cd3762f8ca11b2590800c086b234d3bfffc
Ryan Dens <ryandens735@gmail.com>
2022-09-17 22:26:15 -0700

    Add variant configuration for JRE

--- jlink-gradle/src/main/kotlin/com/ryandens/jlink/JlinkJrePlugin.kt
+++ jlink-gradle/src/main/kotlin/com/ryandens/jlink/JlinkJrePlugin.kt
@@ -3,6 +3,7 @@ package com.ryandens.jlink
 import com.ryandens.jlink.tasks.JlinkJreTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.attributes.Usage
 import org.gradle.api.plugins.JavaPlugin
 
 class JlinkJrePlugin : Plugin<Project> {
@@ -17,8 +18,18 @@ class JlinkJrePlugin : Plugin<Project> {
       modules.convention(listOf("java.base"))
     }
 
-    project.tasks.register(JLINK_JRE_TASK_NAME, JlinkJreTask::class.java) {
+    val jlinkJreTask = project.tasks.register(JLINK_JRE_TASK_NAME, JlinkJreTask::class.java) {
       it.modules.set(extension.modules)
+    }
+    
+    project.configurations.create("jlinkJre") {
+      it.isCanBeConsumed = true
+      it.isCanBeResolved = false
+      it.isVisible = false
+      it.attributes.attribute(Usage.USAGE_ATTRIBUTE, project.objects.named(Usage::class.java, "jlink-jre"))
+      it.outgoing.artifact(jlinkJreTask.map { jreTask ->
+        jreTask.outputDirectory
+      })
     }
   }
 }
