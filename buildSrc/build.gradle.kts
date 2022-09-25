3bcda15e7ad73e541c23016805991de21a7c84b1
Ryan Dens <ryandens735@gmail.com>
2022-09-25 16:12:34 -0700

    Format buildSrc buildscript

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,14 +1,20 @@
 plugins {
-  `kotlin-dsl`
+    `kotlin-dsl`
+    id("com.diffplug.spotless") version "6.11.0"
 }
 
 repositories {
-  gradlePluginPortal()
+    gradlePluginPortal()
 }
 
+spotless {
+    kotlinGradle {
+        ktlint()
+    }
+}
 
 dependencies {
-  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
-  implementation("com.gradle.publish:plugin-publish-plugin:0.20.0")
-  implementation("com.diffplug.spotless:spotless-plugin-gradle:6.11.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
+    implementation("com.gradle.publish:plugin-publish-plugin:0.20.0")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.11.0")
 }
