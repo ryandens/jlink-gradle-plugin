5ae9946e687597b9368e100f391e9c15b1a64b3b
Ryan Dens <ryandens735@gmail.com>
2022-09-25 17:11:55 -0700

    :arrow_up: upgrade plugin publish plugin to 1.0.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -16,6 +16,6 @@ spotless {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
-    implementation("com.gradle.publish:plugin-publish-plugin:0.20.0")
+    implementation("com.gradle.publish:plugin-publish-plugin:1.0.0")
     implementation("com.diffplug.spotless:spotless-plugin-gradle:6.11.0")
 }
