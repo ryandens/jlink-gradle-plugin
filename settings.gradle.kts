e16f429314935eb459da2b4fe31167fc6f4aecd9
Ryan Dens <ryandens735@gmail.com>
2022-12-01 18:42:03 -0800

    Publish scans in CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,26 @@
+plugins {
+  id("com.gradle.enterprise") version "3.11.4"
+}
+
 rootProject.name = "jlink-gradle-plugin"
 include("jlink-gradle")
 include("jlink-jib")
+
+
+val isCI = System.getenv("CI") != null
+
+gradleEnterprise {
+  buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
+    isUploadInBackground = !isCI
+
+    if (isCI) {
+      publishAlways()
+    }
+
+    capture {
+      isTaskInputFiles = true
+    }
+  }
+}
