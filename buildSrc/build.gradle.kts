fcffef4aa07b164e18520394db75b50c356ad101
Ryan Dens <ryandens735@gmail.com>
2022-12-01 20:34:48 -0800

    update buildSrc to depend on plugins based on plugin id

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -15,7 +15,7 @@ spotless {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
-    implementation("com.gradle.publish:plugin-publish-plugin:1.0.0")
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.11.0")
+    implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:1.6.21")
+    implementation("com.gradle.plugin-publish:com.gradle.plugin-publish.gradle.plugin:1.0.0")
+    implementation("com.diffplug.spotless:com.diffplug.spotless.gradle.plugin:6.11.0")
 }
