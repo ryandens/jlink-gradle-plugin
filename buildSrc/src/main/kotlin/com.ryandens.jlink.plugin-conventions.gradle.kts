c10174ce3162c557c8fe13104c42e7df47d59ba5
Ryan Dens <ryandens735@gmail.com>
2022-12-01 18:34:48 -0800

    Specify toolchain

--- buildSrc/src/main/kotlin/com.ryandens.jlink.plugin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/com.ryandens.jlink.plugin-conventions.gradle.kts
@@ -11,6 +11,12 @@ repositories {
     gradlePluginPortal()
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(18))
+    }
+}
+
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
