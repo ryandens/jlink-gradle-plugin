77459d1d56d8125ba8ec056d6f0fb7617d9d4c9e
Marius Volkhart <marius@volkhart.com>
2023-03-13 17:27:47 -0600

    Add variants to Distributions
    Each variant gets a distribution. The variant distributions inherit inputs from the main distribution.

--- jlink-gradle/src/main/kotlin/com/ryandens/jlink/JlinkJreApplicationDistributionPlugin.kt
+++ jlink-gradle/src/main/kotlin/com/ryandens/jlink/JlinkJreApplicationDistributionPlugin.kt
@@ -1,12 +1,16 @@
 package com.ryandens.jlink
 
 import com.ryandens.jlink.tasks.JlinkJreTask
+import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.distribution.Distribution
 import org.gradle.api.distribution.DistributionContainer
 import org.gradle.api.distribution.plugins.DistributionPlugin
+import org.gradle.api.file.Directory
 import org.gradle.api.internal.plugins.WindowsStartScriptGenerator
 import org.gradle.api.plugins.ApplicationPlugin
+import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.application.CreateStartScripts
 
 class JlinkJreApplicationDistributionPlugin : Plugin<Project> {
@@ -14,16 +18,31 @@ class JlinkJreApplicationDistributionPlugin : Plugin<Project> {
         project.pluginManager.apply(ApplicationPlugin::class.java)
         project.pluginManager.apply(JlinkJrePlugin::class.java)
 
-        val jlinkJreOutput = project.tasks.named(JlinkJrePlugin.JLINK_JRE_TASK_NAME, JlinkJreTask::class.java).get().outputDirectory.dir("jre")
+        val distributions = project.extensions.getByType(DistributionContainer::class.java)
+        val mainDistribution = distributions.named(DistributionPlugin.MAIN_DISTRIBUTION_NAME)
+        val mainDistributionContents = mainDistribution.map(Distribution::getContents)
 
-        project.extensions.getByType(DistributionContainer::class.java).named(DistributionPlugin.MAIN_DISTRIBUTION_NAME)
-            .configure { distribution ->
-                distribution.contents { copy ->
-                    copy.from(jlinkJreOutput) {
-                        it.into("jre")
-                    }
+        fun jlinkTaskOutputs(taskName: String): Provider<Directory> =
+            project.tasks.named(taskName, JlinkJreTask::class.java).flatMap { it.outputDirectory.dir("jre") }
+
+        fun configureDistribution(jlinkOutput: Provider<Directory>) = Action<Distribution> { distribution ->
+            distribution.contents { copy ->
+                copy.from(jlinkOutput) {
+                    it.into("jre")
+                }
+                if (distribution.name != DistributionPlugin.MAIN_DISTRIBUTION_NAME) {
+                    copy.with(mainDistributionContents.get())
+                        .exclude { it.file.absolutePath.contains("jlink-jre/jre") }
                 }
             }
+        }
+
+        val jlinkJreOutput = jlinkTaskOutputs(JlinkJrePlugin.JLINK_JRE_TASK_NAME)
+        mainDistribution.configure(configureDistribution(jlinkJreOutput))
+
+        project.extensions.getByType(JlinkJreExtension::class.java).variants.all {
+            distributions.register(it.name, configureDistribution(jlinkTaskOutputs(it.jlinkTaskName)))
+        }
 
         project.tasks.named(ApplicationPlugin.TASK_START_SCRIPTS_NAME, CreateStartScripts::class.java) {
             // custom start script generator that replaces the JAVACMD with one that maps to the jlink JRE java binary
