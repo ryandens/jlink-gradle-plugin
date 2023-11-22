cd9ef1c7470b09b1bcbbc9ff76b2c7ee0fea5d06
Simon Scholz <opensource.simon@gmail.com>
2023-11-22 23:42:44 +0100

    Add KDoc to JlinkAwareUnixStartScriptGenerator and JlinkAwareWindowsStartScriptGenerator

--- jlink-gradle/src/main/kotlin/com/ryandens/jlink/JlinkAwareWindowsStartScriptGenerator.kt
+++ jlink-gradle/src/main/kotlin/com/ryandens/jlink/JlinkAwareWindowsStartScriptGenerator.kt
@@ -4,6 +4,11 @@ import org.gradle.api.internal.plugins.DefaultTemplateBasedStartScriptGenerator
 import org.gradle.api.internal.plugins.StartScriptTemplateBindingFactory
 import org.gradle.util.internal.TextUtil
 
+/**
+ * A custom [org.gradle.jvm.application.scripts.TemplateBasedScriptGenerator]
+ * that replaces the %JAVA_EXE% with one that maps to the jlink JRE java binary.
+ * Also see windowsStartScript.txt in src/main/resources
+ */
 class JlinkAwareWindowsStartScriptGenerator : DefaultTemplateBasedStartScriptGenerator(
     TextUtil.getWindowsLineSeparator(),
     StartScriptTemplateBindingFactory.windows(),
