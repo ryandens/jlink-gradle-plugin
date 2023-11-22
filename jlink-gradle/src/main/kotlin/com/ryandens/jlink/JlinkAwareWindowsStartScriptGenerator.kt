ec648dc3a0151170ae18161643dbaf5ed1149246
Simon Scholz <opensource.simon@gmail.com>
2023-11-23 00:28:26 +0100

    Make use of the proper windowsStartScript.txt template

--- jlink-gradle/src/main/kotlin/com/ryandens/jlink/JlinkAwareWindowsStartScriptGenerator.kt
+++ jlink-gradle/src/main/kotlin/com/ryandens/jlink/JlinkAwareWindowsStartScriptGenerator.kt
@@ -8,6 +8,8 @@ import org.gradle.util.internal.TextUtil
  * A custom [org.gradle.jvm.application.scripts.TemplateBasedScriptGenerator]
  * that replaces the %JAVA_EXE% with one that maps to the jlink JRE java binary.
  * Also see windowsStartScript.txt in src/main/resources
+ * The original source of windowsStartScript.txt can be found here:
+ * https://github.com/gradle/gradle/blob/master/subprojects/plugins/src/main/resources/org/gradle/api/internal/plugins/windowsStartScript.txt
  */
 class JlinkAwareWindowsStartScriptGenerator : DefaultTemplateBasedStartScriptGenerator(
     TextUtil.getWindowsLineSeparator(),
