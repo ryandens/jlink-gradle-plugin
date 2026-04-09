d03a03c4555bfea13ac76fb8971d8093a40d08cc
Ryan Dens <ryandens735@gmail.com>
2026-04-09 00:04:23 -0700

    :bug: fix bugs with Configuration class, allowing instantiation by either plugin

--- jlink-jib/src/main/java/com/ryandens/jlink/jib/Configuration.java
+++ jlink-jib/src/main/java/com/ryandens/jlink/jib/Configuration.java
@@ -4,6 +4,7 @@ import java.nio.file.attribute.PosixFilePermission;
 import javax.inject.Inject;
 import org.gradle.api.Project;
 import org.gradle.api.file.DirectoryProperty;
+import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.provider.SetProperty;
 import org.gradle.api.tasks.Input;
 
@@ -12,17 +13,29 @@ public class Configuration {
   private final DirectoryProperty jlinkJre;
   private final SetProperty<PosixFilePermission> posixPermissions;
 
+  /** Used by jib plugin */
   @Inject
   public Configuration(final Project project) {
-    jlinkJre = project.getObjects().directoryProperty();
-    posixPermissions = project.getObjects().setProperty(PosixFilePermission.class);
+    this(project.getObjects());
   }
 
+  /** Used by tiny-jib */
+  public Configuration(final ObjectFactory objectFactory) {
+    jlinkJre = objectFactory.directoryProperty();
+    posixPermissions = objectFactory.setProperty(PosixFilePermission.class);
+  }
+
+  /**
+   * @return the JRE created by jlink. This is used as the entrypoint for the container.
+   */
   @Input
   DirectoryProperty getJlinkJre() {
     return jlinkJre;
   }
 
+  /**
+   * @return the file permissions to set for the javaagent files being added to the image layer.
+   */
   @Input
   SetProperty<PosixFilePermission> getPosixPermissions() {
     return posixPermissions;
